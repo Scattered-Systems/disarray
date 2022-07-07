18c417be3ad12cd1ba75584acaf29f84f6c0a1cd
FL03 <jo3mccain@icloud.com>
2022-07-07 12:19:20 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- disarray/src/comps/mod.rs
+++ disarray/src/comps/mod.rs
@@ -1,7 +1,10 @@
 /*
-   Appellation: clients
+   Appellation: comps
    Context:
    Creator: FL03 <jo3mccain@icloud.com>
    Description:
        ... Summary ...
 */
+pub use consensus::*;
+
+mod consensus;
