b226baf1f53fdd03e3de11e1bf79ccc3cdda8073
FL03 <jo3mccain@icloud.com>
2022-07-25 21:29:42 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ core/src/lib.rs
@@ -0,0 +1,13 @@
+/*
+   Appellation: disarray-core <library>
+   Creator: FL03 <jo3mccain@icloud.com>
+   Description:
+    ... Summary ...
+*/
+#[doc(inline)]
+pub use crate::{actors::*, components::*, core::*, data::*};
+
+mod actors;
+mod components;
+mod core;
+mod data;
