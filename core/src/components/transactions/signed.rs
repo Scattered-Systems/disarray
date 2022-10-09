855b077ad9a0d6352446695ce1f4ae8fd244ef2c
Joe McCain III <jo3mccain@scattered-systems.com>
2022-10-09 17:44:33 +0000

    Update

--- core/src/components/transactions/signed.rs
+++ core/src/components/transactions/signed.rs
@@ -1,6 +1,13 @@
-
+/*
+   Appellation: signed <module>
+   Contributors: FL03 <jo3mccain@icloud.com> (https://gitlab.com/FL03)
+   Description:
+       ... Summary ...
+*/
 use super::Transaction;
+use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct SignedTransaction {
     transaction: Transaction
 }
