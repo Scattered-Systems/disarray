8afbba5ea840ae77aadb97a2b1eb2b253c8bb52c
Joe McCain III <jo3mccain@icloud.com>
2022-11-06 19:39:49 +0000

    Update

--- network/src/components/providers/provider.rs
+++ network/src/components/providers/provider.rs
@@ -20,7 +20,7 @@ impl Provider {
 
 impl std::fmt::Display for Provider {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Provider(\naddress={:#?})", self.address)
+        write!(f, "{}", self.address)
     }
 }
 
