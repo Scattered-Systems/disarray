4e2229266d8689a5b803f509c9465bf33031f8dd
Joe McCain III <jo3mccain@icloud.com>
2022-12-06 02:02:26 +0000

    Update

--- lib/minis/src/actors/block_fetch/mod.rs
+++ lib/minis/src/actors/block_fetch/mod.rs
@@ -1,10 +1,9 @@
 /*
     Appellation: block_fetch <module>
-    Contributors: FL03 <jo3mccain@icloud.com> (https://gitlab.com/FL03)
-    Description:
-        ... Summary ...
+    Contrib: FL03 <jo3mccain@icloud.com>
+    Description: ... Summary ...
 */
-pub use self::agent::BlockFetch;
+pub use self::{agent::*, state::*};
 
 pub(crate) mod agent;
 
