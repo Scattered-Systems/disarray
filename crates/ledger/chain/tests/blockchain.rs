cdfa42d9b94820b8e6af7587e22ec614167f7207
Joe McCain III <jo3mccain@icloud.com>
2023-01-24 01:21:10 +0000

    Update

--- crates/ledger/chain/tests/blockchain.rs
+++ crates/ledger/chain/tests/blockchain.rs
@@ -1,43 +1,42 @@
 #[cfg(test)]
-mod tests {
-    use decanter::prelude::{generate_random_hash, hasher, H256};
-    use disarray_ledger::blocks::{
-        generate_random_block, generate_random_pos_block, Block, BlockType, CoreBlockWrapperExt,
-    };
-    use disarray_ledger::{Blockchain, ChainWrapper, ChainWrapperExt};
-    use scsys::prelude::Timestamp;
+use decanter::prelude::{generate_random_hash, hasher, H256};
+use disarray_chain::blocks::{
+    generate_random_block, generate_random_pos_block, Block, BlockType, CoreBlockWrapperExt,
+};
+use disarray_chain::{Blockchain, ChainWrapper, ChainWrapperExt};
+use scsys::prelude::Timestamp;
 
-    #[test]
-    fn test_block_gen_random() {
-        let a = generate_random_block(BlockType::PoS, false);
-        let b = generate_random_block(BlockType::PoW, false);
-        assert_eq!(&a, &a);
-        assert_ne!(&a, &b)
-    }
-
-    #[test]
-    fn test_block_hash() {
-        let block = generate_random_pos_block(true);
-        let bhash: H256 = hasher(&block).as_slice().to_owned().into();
-        assert_ne!(bhash, generate_random_hash())
-    }
-
-    #[test]
-    fn test_blockchain_genesis() {
-        let timestamp = Timestamp::default().into();
-        let a = Blockchain::genesis(Block::genesis, timestamp);
-        let b = Blockchain::from(timestamp);
-        assert!(a.contains_hash(a.chain.keys().last().unwrap()));
-        assert!(b.contains_hash(b.chain.keys().last().unwrap()));
-        assert!(a.contains_hash(b.chain.keys().last().unwrap()));
-        assert!(b.contains_hash(a.chain.keys().last().unwrap()));
-    }
-
-    #[test]
-    fn test_blockchain_insert() {
-        let timestamp = Timestamp::default().into();
-        let mut bc = Blockchain::genesis(Block::genesis, timestamp);
-        let a = generate_random_block(BlockType::PoW, false);
-        bc.insert_pow(&a);
-    }
+#[test]
+fn test_block_gen_random() {
+    let a = generate_random_block(BlockType::PoS, false);
+    let b = generate_random_block(BlockType::PoW, false);
+    assert_eq!(&a, &a);
+    assert_ne!(&a, &b)
 }
+
+#[test]
+fn test_block_hash() {
+    let block = generate_random_pos_block(true);
+    let bhash: H256 = hasher(&block).as_slice().to_owned().into();
+    assert_ne!(bhash, generate_random_hash())
+}
+
+#[test]
+fn test_blockchain_genesis() {
+    let timestamp = Timestamp::default().into();
+    let a = Blockchain::genesis(Block::genesis, timestamp);
+    let b = Blockchain::from(timestamp);
+    assert!(a.contains_hash(a.chain.keys().last().unwrap()));
+    assert!(b.contains_hash(b.chain.keys().last().unwrap()));
+    assert!(a.contains_hash(b.chain.keys().last().unwrap()));
+    assert!(b.contains_hash(a.chain.keys().last().unwrap()));
+}
+
+#[test]
+fn test_blockchain_insert() {
+    let timestamp = Timestamp::default().into();
+    let mut bc = Blockchain::genesis(Block::genesis, timestamp);
+    let a = generate_random_block(BlockType::PoW, false);
+    bc.insert_pow(&a);
+}
+
